48d134b2b61b389bf5f3412cb3b0ff339a6378cf
Bobby Reed <bobby.karma_xl@yahoo.com>
2023-03-09 21:38:15 -0600

    added a br in Technologies.js, added an About Me section, and built a mobile-first side-scrolling timeline

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -8,6 +8,7 @@ import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './Techn
 const Technologies = () =>  (
   <Section id="tech">
     <SectionDivider />
+    <br />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
       I've worked with a range of useful technologies in the web development world.
